43138593e7be9c285ba8a83c63309c75128028be
P. Reis <76563803+patrickReiis@users.noreply.github.com>
2024-03-23 23:37:06 -0300

    doc: add link to NIP 11

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 		return
 	}
 
-	// init relay
+	// init relay, see https://github.com/nostr-protocol/nips/blob/master/11.md
 	relay.Info.Name = "countries"
 	relay.Info.Description = "serves notes according to your nationality"
 	relay.Info.Contact = s.RelayContact
